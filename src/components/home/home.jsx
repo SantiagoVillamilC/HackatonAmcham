b65be5dd283fd378321fe609adc5a9f77eec204b
SantiagoVillamilC <santyago4993@gmail.com>
2023-06-15 13:51:39 -0500

    Cambios

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,18 +1,36 @@
-import Style from "./home.module.css";  
+import Style from "./home.module.css";
 
-function Home(){
-    return(
-        <div>
-            <div className={Style.contenedorEnfermedades}>
-                <div>Enfermedad</div>
-                <div>Enfermedad</div>
-                <div>Enfermedad</div>
-                <div>Enfermedad</div>
-                <div>Enfermedad</div>
-                <div>Enfermedad</div>
-            </div>
+function Home() {
+  return (
+    <body>
+      <section>
+        <div className={Style.contenedorAnios}>
+          <div className={Style.contenedorTextoAnios}>
+            <h3>Selecciona el año que te interesa</h3>
+          </div>
+          <div className={Style.contenedorEnfermedades}>
+            <div className={Style.celda}>2019</div>
+            <div className={Style.celda}>2020</div>
+            <div className={Style.celda}>2021</div>
+            <div className={Style.celda}>2022</div>
+            <div className={Style.celda}>2023</div>
+          </div>
         </div>
-    )
+      </section>
+      <section>
+        <div className={Style.contenedor}>
+          <h2>Enfermedad</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            excepturi sed. Voluptatibus iusto sint sit voluptatum reprehenderit
+            fugit id ad, corrupti ut, quasi ullam! Fugiat velit ipsam optio nemo
+            labore.
+          </p>
+          <img src="" alt="grafica" />
+        </div>
+      </section>
+    </body>
+  );
 }
 
 export default Home;
