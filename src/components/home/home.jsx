5566801150cb75847b23f0005a039944ea0222a5
SantiagoVillamilC <santyago4993@gmail.com>
2023-06-15 15:27:56 -0500

    Actual

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,7 +3,7 @@ import Style from "./home.module.css";
 function Home() {
   return (
     <body>
-      <section>
+      <section className={Style.seccionAnios}>
         <div className={Style.contenedorAnios}>
           <div className={Style.contenedorTextoAnios}>
             <h3>Selecciona el año que te interesa</h3>
@@ -19,14 +19,23 @@ function Home() {
       </section>
       <section>
         <div className={Style.contenedor}>
-          <h2>Enfermedad</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-            excepturi sed. Voluptatibus iusto sint sit voluptatum reprehenderit
-            fugit id ad, corrupti ut, quasi ullam! Fugiat velit ipsam optio nemo
-            labore.
-          </p>
-          <img src="" alt="grafica" />
+          <div className={Style.contenedorSeccionIzquierda}>
+            <h2>Enfermedad</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              excepturi sed. Voluptatibus iusto sint sit voluptatum
+              reprehenderit fugit id ad, corrupti ut, quasi ullam! Fugiat velit
+              ipsam optio nemo labore.
+            </p>
+          </div>
+          <div className={Style.contenedorSeccionDerecha}>
+            <img className={Style.imagenTarjeta} src="" alt="grafica" />
+            <div className={Style.contenedorTarjetas}>
+              <div className={Style.Tarjetas}>Cuadro</div>
+              <div className={Style.Tarjetas}>Cuadro</div>
+              <div className={Style.Tarjetas}>Cuadro</div>
+            </div>
+          </div>
         </div>
       </section>
     </body>
